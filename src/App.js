6dd2e14a28352d03f33dc9f68a9898d2ca506f0f
Arnab Chatterjee <arnabc1701@gmail.com>
2021-04-17 13:34:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,114 @@
 import {useState} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Card, Nav, Navbar, NavDropdown, Accordion } from 'react-bootstrap';
+import { Button, Card, Nav, Navbar, NavDropdown, Accordion, Carousel, Jumbotron, Modal, Popover, OverlayTrigger } from 'react-bootstrap';
 import './App.css';
 
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Title as="h3">Popover right</Popover.Title>
+    <Popover.Content>
+      And here's some <strong>amazing</strong> content. It's very engaging.
+      right?
+    </Popover.Content>
+  </Popover>
+);
+
+const Example = () => (
+  <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+    <Button variant="success">Click me to see</Button>
+  </OverlayTrigger>
+);
+
 function App() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-<div>
-    <div className="navbar">
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <div className="navbar">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed = "top">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+          <Nav>
+            <Button variant="outline-danger" style={{margin: "0 0.5em"}}>Danger</Button>
+            <Button variant="outline-danger">Danger</Button>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
+
+    <div className="carousel">
+      <Carousel fade>
+        <Carousel.Item interval = {1500}>
+          <img
+            className="d-block w-100"
+            src="https://images.alphacoders.com/944/thumb-1920-944924.png"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval = {1500}>
+          <img
+            className="d-block w-100"
+            src="https://wallpapercave.com/wp/qxOuRkt.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval = {1500}>
+          <img
+            className="d-block w-100"
+            src="https://wallpaperaccess.com/full/641607.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval = {1500}>
+          <img
+            className="d-block w-100"
+            src="https://wallpapercave.com/wp/qxOuRkt.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      </div>
+
+      
+    <Jumbotron>
+      <h1>Hello, world!</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling
+        extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="warning">Learn more</Button>
+      </p>
+    </Jumbotron>
 
       <div className="bcard">
         <Card>
@@ -54,7 +132,8 @@ function App() {
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="primary" onClick={handleShow}>Show me</Button>
+            <Example/>
           </Card.Body>
         </Card>
       </div>
@@ -79,6 +158,30 @@ function App() {
           </Card>
         </Accordion>
       </div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood 🙏😶</Button>
+        </Modal.Footer>
+      </Modal>
+
+
     </div>
   );
 }
